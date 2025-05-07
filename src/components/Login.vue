638f0ea3c617ff87379cc3454f38c9218c1c2772
Snow Ball <1910058487@qq.com>
2025-05-07 22:38:00 +0800

    Project init

---
+++ src/components/Login.vue
@@ -0,0 +1,23 @@
+<!--登录-->
+<template>
+  <h1>{{ title }}</h1>
+  <button @click="fetchData">发送请求</button>
+  <pre>{{ response }}<</pre>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+import axios from 'axios'
+
+const title = ref('登录')
+const response = ref(null)
+const fetchData = async () => {
+  try {
+    const res = await axios.get('https://localhost:3721/test')
+    console.log(res)
+    response.value = res.data
+  } catch (error) {
+    response.value = error
+  }
+}
+</script>
