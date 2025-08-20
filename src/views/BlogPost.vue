a65e7846ed22e37b2d17395d36c0c5ffe0a4efd6
wanderingverse <lh19890781942@126.com>
2025-08-20 12:02:23 +0800

    - 引入 md-editor-v3 依赖用于预览和编辑 Markdown

--- src/views/BlogPost.vue
+++ src/views/BlogPost.vue
@@ -1,8 +1,18 @@
 <script setup>
+import {ref} from 'vue';
+import {MdPreview, MdCatalog} from 'md-editor-v3';
+import 'md-editor-v3/lib/preview.css';
 
+const id = 'preview-only';
+const text = ref('## 测试markdown 内容和目录');
+const scrollElement = document.documentElement;
 </script>
 
 <template>
+  <div style="display: flex;width: 100%;height: 100vh;">
+    <MdCatalog style="flex: 1;" :editorId="id" :scrollElement="scrollElement"/>
+    <MdPreview style="flex: 3;" :id="id" :modelValue="text"/>
+  </div>
 </template>
 
 <style scoped>
